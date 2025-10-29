4e8ca223498562203414b8724a4500e0c05e1b38
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-10-29 21:58:55 +0200

    Luentotehtava 1.1 valmis

---
+++ H1_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cout << "Nimesi on: ";
+    cin >> name;
+    cout << "Ikasi: ";
+    cin >> age;
+    cout << "Terve " << name << ". Olet siis " << age << " vuotias." << endl;
+    return 0;
+}
