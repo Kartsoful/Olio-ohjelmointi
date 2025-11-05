2184c19023897a1f39895320da439902769b0939
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-05 20:57:14 +0200

    H1_5 valmis

---
+++ H1_5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int tuplaa(int luku);
+void luvuntulostus(int luku);
+
+int main() {
+    int x;
+    cout << "Anna kokonaisluku: " << endl;
+    cin >> x;
+    int tupla = tuplaa(x);
+    luvuntulostus(tupla);
+}
+
+int tuplaa(int luku) { return luku * 2; }
+
+void luvuntulostus(int luku) {
+    cout << "Antamasi luku on " << luku << endl;
+}
