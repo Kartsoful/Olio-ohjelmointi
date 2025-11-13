53ab6f46b42dca5211db8a36c61836ac5e0e6cc8
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-13 13:52:03 +0200

    perinta_esim1

--- perinta_esim1/main.cpp
+++ perinta_esim1/main.cpp
@@ -1,6 +1,5 @@
 #include "aliluokka1.h"
 #include <iostream>
-
 using namespace std;
 
 int main()
@@ -8,5 +7,10 @@ int main()
     Aliluokka1 objectAliluokka;
     objectAliluokka.asetaNumerot(3,5,7);
     objectAliluokka.tulostaNumerot();
+
+    Kantaluokka objectKantaluokka;
+    //objectKantaluokka pääsee käsiksi
+    //vain public tyyppisiin ominaisuuksiin
+
     return 0;
 }
